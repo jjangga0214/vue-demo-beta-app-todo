4c96fbd346f057c832c030f2d27e9fce65d7d407
jjangga <jjangga@kookmin.ac.kr>
2018-09-04 13:02:56 +0900

    add TodoInput

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>input</div>
+  <div>
+    <span>{{mark}}</span>
+    <br>
+    <input type="text" v-model="newTask" placeholder="입력해주세요">
+    <button v-on:click="addTask">추가</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Input"
-    }
+  export default {
+    name: "Input",
+    data() {
+      return {
+        newTask: '',
+      };
+    },
+    methods: {
+      addTask() {
+        const key = this.newTask.trim();
+        if (key) {
+          console.log(key + " : " + this.mark);
+          localStorage.setItem(key, this.mark);
+        }
+        this.clearInput();
+      },
+      clearInput() {
+        this.newTask = '';
+      },
+    },
+    computed: {
+      mark() {
+        return this.newTask + '!';
+      },
+    },
+  }
 </script>
 
 <style scoped lang="scss">
