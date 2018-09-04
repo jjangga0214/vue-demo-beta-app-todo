408dd97118d79538381a6864a55398bc1678f71a
jjangga <jjangga@kookmin.ac.kr>
2018-09-04 20:30:28 +0900

    add TodoList reading from localStorage

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="task in tasks">{{task}}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
   export default {
-    name: "List"
+    name: "List",
+    data() {
+      return {
+        tasks: [],
+      };
+    },
+    created() {
+      for (let i = 0; i < localStorage.length; i++) {
+        this.tasks.push(localStorage.getItem(localStorage.key(i)))
+      }
+    },
   }
 </script>
 
