3eefa6186878484d4ee36c5daf6d9880df008a01
jjangga <jjangga@kookmin.ac.kr>
2018-09-05 10:44:42 +0900

    add TodoFooter

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>footer</div>
+  <div class="btn clear-btn shadow" type="button" v-on:click="clear">Clear All</div>
 </template>
 
 <script>
-    export default {
-        name: "Footer"
+  export default {
+    name: "Footer",
+    methods: {
+      clear() {
+        console.log('clear');
+        localStorage.clear()
+      }
     }
+  }
 </script>
 
 <style scoped lang="scss">
+  $height: 50px;
+  * {
+    color: red;
+    color: #E20303;
+    background-color: white;
+    height: $height;
+    line-height: $height;
+    max-width: 200px;
+    margin: 0 auto;
+    border-radius: $height * 0.1;
 
+  }
 </style>
